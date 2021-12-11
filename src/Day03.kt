c7e79587de4b9b0bcc6b9cb5640bf1788d54aad6
Cesar Bretana <bretanac@gmail.com>
2021-12-11 21:54:16 +0100

    feat: day 3 part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,32 @@
 fun main() {
     fun part1(matrix: List<List<Char>>): Int {
         val lenNum = matrix.first().size
-        val gammaRate = List(lenNum) { '0' }
+        val gammaRate = Array(lenNum) { 0 }
+        val epsilonRate = Array(lenNum) { 0 }
 
         for (i in 0 until lenNum) {
             var countZeroes = 0
             var countOnes = 0
             for (j in matrix.indices) {
-                when(matrix[j][i]) {
+                when (matrix[j][i]) {
                     '0' -> countZeroes++
                     else -> countOnes++
                 }
             }
-
-            println("*****************************")
+            if (countOnes > countZeroes) {
+                gammaRate[i] = 1
+            } else {
+                epsilonRate[i] = 1
+            }
         }
 
-        return 0
+        val gammaBinary = gammaRate.toList().joinToString("")
+        val epsilonBinary = epsilonRate.toList().joinToString("")
+
+        val gammaVal = Integer.parseInt(gammaBinary, 2)
+        val epsilonVal = Integer.parseInt(epsilonBinary, 2)
+
+        return gammaVal * epsilonVal
     }
 
     fun part2(matrix: List<Pair<String, Int>>): Int {
@@ -25,5 +35,5 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val matrix = readInput("input").map { it.toList() }
-    part1(matrix)
+    print(part1(matrix))
 }
